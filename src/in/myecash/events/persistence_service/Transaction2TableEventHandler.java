e46d34f74903fb2ebdf2bec4c5af13e946d71861
aditya-gangwar <aditya_gang@yahoo.com>
2016-12-28 23:49:42 +0530

    25 Dec

---
+++ src/in/myecash/events/persistence_service/Transaction2TableEventHandler.java
@@ -0,0 +1,33 @@
+package in.myecash.events.persistence_service;
+
+import com.backendless.servercode.ExecutionResult;
+import com.backendless.servercode.RunnerContext;
+import com.backendless.servercode.annotation.Asset;
+import com.backendless.servercode.annotation.Async;
+
+import in.myecash.common.database.Transaction;
+
+/**
+* Transaction2TableEventHandler handles events for all entities. This is accomplished
+* with the @Asset( "Transaction2" ) annotation.
+* The methods in the class correspond to the events selected in Backendless
+* Console.
+*/
+@Asset( "Transaction2" )
+public class Transaction2TableEventHandler extends com.backendless.servercode.extension.PersistenceExtender<Transaction>
+{
+    @Override
+    public void beforeCreate( RunnerContext context, Transaction transaction) throws Exception
+    {
+        TxnProcessHelper txnEventHelper = new TxnProcessHelper();
+        txnEventHelper.handleTxnCommit(context.getUserToken(), context.getUserId(), transaction, false, false);
+    }
+
+    @Async
+    @Override
+    public void afterCreate( RunnerContext context, Transaction transaction, ExecutionResult<Transaction> result ) throws Exception
+    {
+        TxnProcessHelper txnEventHelper = new TxnProcessHelper();
+        txnEventHelper.handleAfterCreate(context, transaction, result);
+    }
+}
